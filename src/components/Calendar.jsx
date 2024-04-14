ab2b25b622c0f6fadc8eab0a98cfb7503610543b
alweb29 <albertow55@gmail.com>
2024-04-14 11:40:04 +0200

    added base version of calendar view

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,111 @@
+import "../../src/components/Calendar.css";
+
+function Calendar() {
+  return (
+    <div className="calendar">
+      <h1>Kalendarz rezerwacji</h1>
+      <div className="calendar-header">
+        <button>lewo</button>
+        <h1>Maj</h1>
+        <button>prawo</button>
+      </div>
+      <section className="section">
+        <div className="tile booked">
+          <h3>1</h3>
+        </div>
+        <div className="tile">
+          <h3>2</h3>
+        </div>
+        <div className="tile">
+          <h3>3</h3>
+        </div>
+        <div className="tile">
+          <h3>4</h3>
+        </div>
+        <div className="tile">
+          <h3>5</h3>
+        </div>
+        <div className="tile">
+          <h3>6</h3>
+        </div>
+        <div className="tile">
+          <h3>7</h3>
+        </div>
+        <div className="tile">
+          <h3>8</h3>
+        </div>
+        <div className="tile">
+          <h3>9</h3>
+        </div>
+        <div className="tile">
+          <h3>10</h3>
+        </div>
+        <div className="tile">
+          <h3>11</h3>
+        </div>
+        <div className="tile">
+          <h3>12</h3>
+        </div>
+        <div className="tile">
+          <h3>13</h3>
+        </div>
+        <div className="tile">
+          <h3>14</h3>
+        </div>
+        <div className="tile">
+          <h3>15</h3>
+        </div>
+        <div className="tile">
+          <h3>16</h3>
+        </div>
+        <div className="tile">
+          <h3>17</h3>
+        </div>
+        <div className="tile">
+          <h3>18</h3>
+        </div>
+        <div className="tile">
+          <h3>19</h3>
+        </div>
+        <div className="tile">
+          <h3>20</h3>
+        </div>
+        <div className="tile">
+          <h3>21</h3>
+        </div>
+        <div className="tile">
+          <h3>22</h3>
+        </div>
+        <div className="tile">
+          <h3>23</h3>
+        </div>
+        <div className="tile">
+          <h3>24</h3>
+        </div>
+        <div className="tile">
+          <h3>25</h3>
+        </div>
+        <div className="tile">
+          <h3>26</h3>
+        </div>
+        <div className="tile">
+          <h3>27</h3>
+        </div>
+        <div className="tile">
+          <h3>28</h3>
+        </div>
+        <div className="tile">
+          <h3>29</h3>
+        </div>
+        <div className="tile">
+          <h3>30</h3>
+        </div>
+        <div className="tile">
+          <h3>31</h3>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Calendar;
